a2bd5948931b7e09a2afe18de28a69f2ac72a32c
jagi <luke.jagodzinski@gmail.com>
2015-01-05 16:08:06 +0100

    Use textAling in calculations

--- measuretext.js
+++ measuretext.js
@@ -1,5 +1,5 @@
 (function() {
-  var isNative = function () {
+  var isNative = function() {
     var canvas = document.createElement('canvas');
     var ctx = canvas.getContext('2d');
     var metrics = ctx.measureText(' ');
@@ -8,121 +8,208 @@
     return count > 1;
   };
 
-  // Check wheater ther is native support for extended TextMetrics.
+  // Check wheater there is native support for extended TextMetrics.
   if (isNative()) return;
 
   // Store the old text metrics function on the Canvas2D prototype.
   CanvasRenderingContext2D.prototype.measureTextWidth = CanvasRenderingContext2D.prototype.measureText;
 
-  /**
-   * The new text metric object constructor
-   */
-  TextMetrics = function(ctx, text) {
-    this._ctx = ctx;
-    this._text = text;
-    this._isSpace = !(/\S/.test(text));
-    this._metrics = {};
+  var CanvasTextMetrics = function(metrics) {
+    this.metrics = metrics;
   };
 
-  var prepareMetrics = function() {
-    if (this._metricsPrepared) return;
-
-    // If we're not dealing with white space, we can compute metrics.
-    var font = this._ctx.font.split('px ');
-    var fontSize = font[0];
-    var fontFamily = font[1].replace(/"|'/g, '');
+  CanvasTextMetrics.prototype.prepare = function() {
+    if (this._prepared) return;
 
     // Have characters, so measure the text.
     var canvas = document.createElement('canvas');
-    this._padding = 100;
-    canvas.width = this.width + this._padding;
-    canvas.height = 3 * fontSize;
+    this._padding = this.metrics._fontSize / 2;
+    canvas.width = this.getWidth() + (this._padding * 2);
+    canvas.height = 3 * this.metrics._fontSize;
     var ctx = canvas.getContext('2d');
-    ctx.font = fontSize + 'px ' + fontFamily;
+    ctx.font = this.metrics._fontSize + 'px ' + this.metrics._fontFamily;
 
-    this._width = canvas.width;
-    this._height = canvas.height;
-    this._baseline = this._height / 2;
+    this._canvasWidth = canvas.width;
+    this._canvasHeight = canvas.height;
 
     // Set all canvas pixeldata values to 255, with all the content
     // data being 0. This lets us scan for data[i] != 255.
     ctx.fillStyle = 'white';
-    ctx.fillRect(-1, -1, this._width + 2, this._height + 2);
+    ctx.fillRect(-1, -1, this._canvasWidth + 2, this._canvasHeight + 2);
+    this._textPosX;
+    this._textPosY = this._canvasHeight / 2;
+    switch (this.metrics._textAlign) {
+      case 'center':
+        this._textPosX = this._canvasWidth / 2;
+        break;
+      case 'right':
+      case 'end':
+        this._textPosX = this._canvasWidth - this._padding;
+        break;
+      case 'left':
+      case 'start':
+      default:
+        this._textPosX = this._padding;
+        break;
+    }
     ctx.fillStyle = 'black';
-    ctx.fillText(this._text, this._padding / 2, this._baseline);
-    this._pixelData = ctx.getImageData(0, 0, this._width, this._height).data;
+    ctx.textAlign = this.metrics._textAlign;
+    ctx.fillText(this.metrics._text, this._textPosX, this._textPosY);
+    this._pixelData = ctx.getImageData(0, 0, this._canvasWidth, this._canvasHeight).data;
 
-    this._metricsPrepared = true;
+    this._prepared = true;
+  };
+
+  CanvasTextMetrics.prototype.getWidth = function() {
+    if (!this.metrics._cache.hasOwnProperty('width')) {
+      var canvas = document.createElement('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.font = this.metrics._font;
+      this.metrics._cache.width = ctx.measureTextWidth(this.metrics._text).width;
+    }
+
+    return this.metrics._cache.width;
+  };
+
+  CanvasTextMetrics.prototype.getActualBoundingBoxLeft = function() {
+    if (!this.metrics._cache.hasOwnProperty('actualBoundingBoxLeft')) {
+      if (this.metrics._isSpace) {
+        this.metrics._cache.actualBoundingBoxLeft = 0;
+      } else {
+        this.prepare();
+
+        var i = 0;
+        var w4 = this._canvasWidth * 4;
+        var l = this._pixelData.length;
+
+        // Find the min-x coordinate.
+        for (var i = 0; i < l && this._pixelData[i] === 255;) {
+          i += w4;
+          if (i >= l) {
+            i = (i - l) + 4;
+          }
+        }
+        var minx = ((i % w4) / 4) | 0;
+
+        this.metrics._cache.actualBoundingBoxLeft = this._textPosX - minx;
+      }
+    }
+
+    return this.metrics._cache.actualBoundingBoxLeft;
+  };
+
+  CanvasTextMetrics.prototype.getActualBoundingBoxRight = function() {
+    if (!this.metrics._cache.hasOwnProperty('actualBoundingBoxRight')) {
+      if (this.metrics._isSpace) {
+        this.metrics._cache.actualBoundingBoxRight = this.width;
+      } else {
+        this.prepare();
+
+        var i = 0;
+        var w4 = this._canvasWidth * 4;
+        var l = this._pixelData.length;
+        var step = 1;
+
+        // Find the max-x coordinate.
+        for (i = l - 3; i >= 0 && this._pixelData[i] === 255;) {
+          i -= w4;
+          if (i < 0) {
+            i = (l - 3) - (step++) * 4;
+          }
+        }
+        var maxx = ((i % w4) / 4) + 1 | 0;
+
+        this.metrics._cache.actualBoundingBoxRight = maxx - this._textPosX;
+      }
+    }
+
+    return this.metrics._cache.actualBoundingBoxRight;
+  };
+
+  CanvasTextMetrics.prototype.getActualBoundingBoxAscent = function() {
+    if (!this.metrics._cache.hasOwnProperty('actualBoundingBoxAscent')) {
+      if (this.metrics._isSpace) {
+        this.metrics._cache.actualBoundingBoxAscent = 0;
+      } else {
+        this.prepare();
+
+        var i = 0;
+        var w4 = this._canvasWidth * 4;
+        var l = this._pixelData.length;
+
+        // Finding the ascent uses a normal, forward scanline.
+        while (++i < l && this._pixelData[i] === 255) {}
+        var ascent = (i / w4) | 0;
+
+        this.metrics._cache.actualBoundingBoxAscent = this._textPosY - ascent;
+      }
+    }
+
+    return this.metrics._cache.actualBoundingBoxAscent;
+  };
+
+  CanvasTextMetrics.prototype.getActualBoundingBoxDescent = function() {
+    if (!this.metrics._cache.hasOwnProperty('actualBoundingBoxDescent')) {
+      if (this.metrics._isSpace) {
+        this.metrics._cache.actualBoundingBoxDescent = 0;
+      } else {
+        this.prepare();
+
+        var w4 = this._canvasWidth * 4;
+        var l = this._pixelData.length;
+        var i = l - 1;
+
+        // Finding the descent uses a reverse scanline.
+        while (--i > 0 && this._pixelData[i] === 255) {}
+        var descent = (i / w4) | 0;
+
+        this.metrics._cache.actualBoundingBoxDescent = descent - this._textPosY;
+      }
+    }
+
+    return this.metrics._cache.actualBoundingBoxDescent;
+  };
+
+  var SVGTextMetrics = function(metrics) {
+    this.metrics = metrics;
+  };
+
+  /**
+   * The new text metric object constructor
+   */
+  var TextMetrics = function(ctx, text) {
+    this._text = text;
+    this._isSpace = !(/\S/.test(text));
+
+    var font = ctx.font.split('px ');
+    this._font = ctx.font;
+    this._fontSize = parseInt(font[0], 10);
+    this._fontFamily = font[1].replace(/"|'/g, '');
+    this._textAlign = ctx.textAlign;
+
+    this._cache = {};
+
+    this._canvasMetrics = new CanvasTextMetrics(this);
+    this._svgMetrics = new SVGTextMetrics(this);
   };
 
   TextMetrics.prototype = {};
 
   Object.defineProperty(TextMetrics.prototype, 'width', {
     get: function() {
-      if (!this._metrics.hasOwnProperty('width')) {
-        this._metrics.width = this._ctx.measureTextWidth(this._text).width;
-      }
-
-      return this._metrics.width;
+      return this._canvasMetrics.getWidth();
     }
   });
 
   Object.defineProperty(TextMetrics.prototype, 'actualBoundingBoxLeft', {
     get: function() {
-      if (!this._metrics.hasOwnProperty('actualBoundingBoxLeft')) {
-        if (this._isSpace) {
-          this._metrics.actualBoundingBoxLeft = 0;
-        } else {
-          prepareMetrics.call(this);
-
-          var i = 0;
-          var w4 = this._width * 4;
-          var l = this._pixelData.length;
-
-          // Find the min-x coordinate.
-          for (var i = 0; i < l && this._pixelData[i] === 255;) {
-            i += w4;
-            if (i >= l) {
-              i = (i - l) + 4;
-            }
-          }
-          var minx = ((i % w4) / 4) | 0;
-
-          this._metrics.actualBoundingBoxLeft = minx - (this._padding / 2);
-        }
-      }
-
-      return this._metrics.actualBoundingBoxLeft;
+      return this._canvasMetrics.getActualBoundingBoxLeft();
     }
   });
 
   Object.defineProperty(TextMetrics.prototype, 'actualBoundingBoxRight', {
     get: function() {
-      if (!this._metrics.hasOwnProperty('actualBoundingBoxRight')) {
-        if (this._isSpace) {
-          this._metrics.actualBoundingBoxRight = this.width;
-        } else {
-          prepareMetrics.call(this);
-
-          var i = 0;
-          var w4 = this._width * 4;
-          var l = this._pixelData.length;
-          var step = 1;
-
-          // Find the max-x coordinate.
-          for (i = l - 3; i >= 0 && this._pixelData[i] === 255;) {
-            i -= w4;
-            if (i < 0) {
-              i = (l - 3) - (step++) * 4;
-            }
-          }
-          var maxx = ((i % w4) / 4) + 1 | 0;
-
-          this._metrics.actualBoundingBoxRight = maxx - (this._padding / 2);
-        }
-      }
-
-      return this._metrics.actualBoundingBoxRight;
+      return this._canvasMetrics.getActualBoundingBoxRight();
     }
   });
 
@@ -140,94 +227,58 @@
 
   Object.defineProperty(TextMetrics.prototype, 'actualBoundingBoxAscent', {
     get: function() {
-      if (!this._metrics.hasOwnProperty('actualBoundingBoxAscent')) {
-        if (this._isSpace) {
-          this._metrics.actualBoundingBoxAscent = 0;
-        } else {
-          prepareMetrics.call(this);
-
-          var i = 0;
-          var w4 = this._width * 4;
-          var l = this._pixelData.length;
-
-          // Finding the ascent uses a normal, forward scanline.
-          while (++i < l && this._pixelData[i] === 255) {}
-          var ascent = (i / w4) | 0;
-
-          this._metrics.actualBoundingBoxAscent = this._baseline - ascent;
-        }
-      }
-
-      return this._metrics.actualBoundingBoxAscent;
+      return this._canvasMetrics.getActualBoundingBoxAscent();
     }
   });
 
   Object.defineProperty(TextMetrics.prototype, 'actualBoundingBoxDescent', {
     get: function() {
-      if (!this._metrics.hasOwnProperty('actualBoundingBoxDescent')) {
-        if (this._isSpace) {
-          this._metrics.actualBoundingBoxDescent = 0;
-        } else {
-          prepareMetrics.call(this);
-
-          var w4 = this._width * 4;
-          var l = this._pixelData.length;
-          var i = l - 1;
-
-          // Finding the descent uses a reverse scanline.
-          while (--i > 0 && this._pixelData[i] === 255) {}
-          var descent = (i / w4) | 0;
-
-          this._metrics.actualBoundingBoxDescent = descent - this._baseline;
-        }
-      }
-
-      return this._metrics.actualBoundingBoxDescent;
+      return this._canvasMetrics.getActualBoundingBoxDescent();
     }
   });
 
   Object.defineProperty(TextMetrics.prototype, 'emHeightAscent', {
     get: function() {
-      if (!this._metrics.hasOwnProperty('emHeightAscent')) {
-        this._metrics.emHeightAscent = 'top';
+      if (!this.metrics._cache.hasOwnProperty('emHeightAscent')) {
+        this.metrics._cache.emHeightAscent = 'top';
       }
-      return this._metrics.emHeightAscent;
+      return this.metrics._cache.emHeightAscent;
     }
   });
 
   Object.defineProperty(TextMetrics.prototype, 'emHeightDescent', {
     get: function() {
-      if (!this._metrics.hasOwnProperty('emHeightDescent')) {
-        this._metrics.emHeightDescent = 'bottom';
+      if (!this.metrics._cache.hasOwnProperty('emHeightDescent')) {
+        this.metrics._cache.emHeightDescent = 'bottom';
       }
-      return this._metrics.emHeightDescent;
+      return this.metrics._cache.emHeightDescent;
     }
   });
 
   Object.defineProperty(TextMetrics.prototype, 'hangingBaseline', {
     get: function() {
-      if (!this._metrics.hasOwnProperty('hangingBaseline')) {
-        this._metrics.hangingBaseline = 'hanging';
+      if (!this.metrics._cache.hasOwnProperty('hangingBaseline')) {
+        this.metrics._cache.hangingBaseline = 'hanging';
       }
-      return this._metrics.hangingBaseline;
+      return this.metrics._cache.hangingBaseline;
     }
   });
 
   Object.defineProperty(TextMetrics.prototype, 'alphabeticBaseline', {
     get: function() {
-      if (!this._metrics.hasOwnProperty('alphabeticBaseline')) {
-        this._metrics.alphabeticBaseline = 'alphabetic';
+      if (!this.metrics._cache.hasOwnProperty('alphabeticBaseline')) {
+        this.metrics._cache.alphabeticBaseline = 'alphabetic';
       }
-      return this._metrics.alphabeticBaseline;
+      return this.metrics._cache.alphabeticBaseline;
     }
   });
 
   Object.defineProperty(TextMetrics.prototype, 'ideographicBaseline', {
     get: function() {
-      if (!this._metrics.hasOwnProperty('ideographicBaseline')) {
-        this._metrics.ideographicBaseline = 'ideographic';
+      if (!this.metrics._cache.hasOwnProperty('ideographicBaseline')) {
+        this.metrics._cache.ideographicBaseline = 'ideographic';
       }
-      return this._metrics.ideographicBaseline;
+      return this.metrics._cache.ideographicBaseline;
     }
   });
 
